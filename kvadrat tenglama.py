6990a53a3208b2f7b7700da8fca84b31b7634fe0
Fotihbek Komilov <95439454+Fotihbek@users.noreply.github.com>
2022-02-09 18:27:16 +0500

    Update kvadrat tenglama.py

--- kvadrat tenglama.py
+++ kvadrat tenglama.py
@@ -1,8 +1,21 @@
+import math
+
 a = int(input('a qiymatni kiriting- '))
 b = int(input('b qiymatni kiriting- '))
 c = int(input("c qiymatni kiriting- "))
-d = (b**2 - 4*a*c)**(1/2)
-x1 = (-b + d)/(2*a)
-x2 = (-b - d)/(2*a)
-print('x1 = {}'.format(x1))
-print('x2 = {}'.format(x2))
+d = b**2 - 4*a*c
+
+if d > 0:
+    x1 = (-b + math.sqrt(d))/(2*a)
+    x2 = (-b - math.sqrt(d))/(2*a)
+    print("Tenglama 2 ta ildizga ega!")
+    print('x1 = {}'.format(x1))
+    print('x2 = {}'.format(x2))
+
+elif d == 0:
+    x = (-b + math.sqrt(d))/(2*a)
+    print("Tenglama 1 ta ildizga ega!")
+    print('x = {}'.format(x))
+
+else:
+    print("Tenglama haqiqiy ildizga ega emas!")
